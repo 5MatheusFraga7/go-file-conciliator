4fffaade7e9edbc9798cc8eaf871befdd289da20
5MatheusFraga7 <fragamatheus823@gmail.com>
2024-05-03 02:45:05 -0300

    Removendo banco e separando lógica em conciliator

--- cmd/main.go
+++ cmd/main.go
@@ -2,98 +2,22 @@ package main
 
 import (
 	"database-conciliator/internal/conciliator"
-	"encoding/csv"
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
-	"sync"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// internalData, externalData := getDataToConciliate()
+	internalFile, externalFile := getFilesToConciliate()
 
-	// dataDiff := getBynaryFileDiff(internalData, externalData)
-
-	// writeCSV("output_diff.csv", dataDiff)
-
-	c := conciliator.Conciliator{File: getFileToConciliate()}
+	c := conciliator.Conciliator{InternalFile: internalFile, ExternalFile: externalFile}
 	c.Conciliate()
-
 }
 
-func getBynaryFileDiff(internalData [][]string, externalData [][]string) [][]string {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	diff := [][]string{}
-
-	maxGoroutines := 100
-	goroutines := make(chan struct{}, maxGoroutines)
-
-	for i := 0; i < len(internalData); i++ {
-		wg.Add(1)
-		goroutines <- struct{}{}
-
-		go func(idx int) {
-			defer wg.Done()
-			defer func() { <-goroutines }()
-
-			if !binarySearch(internalData[idx], externalData) {
-				mu.Lock()
-				diff = append(diff, internalData[idx])
-				mu.Unlock()
-			}
-		}(i)
-	}
-	wg.Wait()
-	return diff
-}
-
-func binarySearch(value []string, array [][]string) bool {
-	left := 0
-	right := len(array) - 1
-
-	target, _ := strconv.Atoi(value[0])
-
-	for left <= right {
-		mid := left + (right-left)/2
-		num, _ := strconv.Atoi(array[mid][0])
-
-		if num == target {
-			return true
-		} else if target < num {
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-
-	return false
-}
-
-func writeCSV(filename string, data [][]string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	for _, row := range data {
-		if err := writer.Write(row); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func getFileToConciliate() *os.File {
+func getFilesToConciliate() (*os.File, *os.File) {
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
@@ -110,11 +34,13 @@ func getFileToConciliate() *os.File {
 		log.Fatalf("Erro ao carregar arquivo .env: %v", err)
 	}
 
-	filePath := os.Getenv("FILE_PATH")
+	filePath := os.Getenv("INTERNAL_FILE_PATH")
+	filePath2 := os.Getenv("EXTERNAL_FILE_PATH")
 
-	file, err := os.Open(filePath)
+	file1, err := os.Open(filePath)
+	file2, err := os.Open(filePath2)
 
-	return file
+	return file1, file2
 }
 
 func getDataToConciliate() ([][]string, [][]string) {
--- internal/conciliator/conciliator.go
+++ internal/conciliator/conciliator.go
@@ -1,30 +1,29 @@
 package conciliator
 
 import (
-	"database-conciliator/internal/db"
-	"database-conciliator/internal/db/adapters"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
+	"sync"
 )
 
 type Conciliator struct {
-	File *os.File
+	InternalFile *os.File
+	ExternalFile *os.File
 }
 
 func (c *Conciliator) Conciliate() {
-	startDatabase()
-
 	records := c.GetDataFromInternalFile()
+	records2 := c.GetDataFromExternalFile()
 
-	for _, row := range records {
-		fmt.Println(row)
-	}
+	dataDiff := getBynaryFileDiff(records, records2)
 
+	writeCSV("output_diff.csv", dataDiff)
 }
 
 func (c *Conciliator) GetDataFromInternalFile() [][]string {
-	file := c.File
+	file := c.InternalFile
 	defer file.Close()
 
 	reader := csv.NewReader(file)
@@ -37,13 +36,84 @@ func (c *Conciliator) GetDataFromInternalFile() [][]string {
 	return records
 }
 
-func startDatabase() {
-	postgresAdapter := adapters.NewPostgreSQLAdapter()
-	err := db.OpenConnectionToDatabase(postgresAdapter)
-	defer db.CloseConnectionToDatabase(postgresAdapter)
+func (c *Conciliator) GetDataFromExternalFile() [][]string {
+	file := c.ExternalFile
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error reading records")
 	}
 
+	return records
+}
+
+func getBynaryFileDiff(internalData [][]string, externalData [][]string) [][]string {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	diff := [][]string{}
+
+	maxGoroutines := 100
+	goroutines := make(chan struct{}, maxGoroutines)
+
+	for i := 0; i < len(internalData); i++ {
+		wg.Add(1)
+		goroutines <- struct{}{}
+
+		go func(idx int) {
+			defer wg.Done()
+			defer func() { <-goroutines }()
+
+			if !binarySearch(internalData[idx], externalData) {
+				mu.Lock()
+				diff = append(diff, internalData[idx])
+				mu.Unlock()
+			}
+		}(i)
+	}
+	wg.Wait()
+	return diff
+}
+
+func binarySearch(value []string, array [][]string) bool {
+	left := 0
+	right := len(array) - 1
+
+	target, _ := strconv.Atoi(value[0])
+
+	for left <= right {
+		mid := left + (right-left)/2
+		num, _ := strconv.Atoi(array[mid][0])
+
+		if num == target {
+			return true
+		} else if target < num {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return false
+}
+
+func writeCSV(filename string, data [][]string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, row := range data {
+		if err := writer.Write(row); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
